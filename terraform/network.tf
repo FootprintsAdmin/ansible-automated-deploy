40458cf5dcce9a65d4930f3ed60f3c60f1a804e0
Mitch Joa <mitch.joa@yello.co>
2017-09-20 11:59:43 -0500

    Adjust logstash security group

--- terraform/network.tf
+++ terraform/network.tf
@@ -136,9 +136,9 @@ resource "aws_security_group" "logstash_sg" {
   }
 
   ingress {
-    from_port = 9600
-    to_port = 9700
-    protocol = "tcp"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
