f1991fe789f60b56579d47563543ab6f29e036bd
Trent Kan <trent.kan@yello.co>
2017-09-25 12:24:20 -0500

    Use new host for elasticsearch

--- terraform/network.tf
+++ terraform/network.tf
@@ -136,9 +136,9 @@ resource "aws_security_group" "logstash_sg" {
   }
 
   ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port = 9600
+    to_port = 9600
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
